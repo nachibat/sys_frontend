e1af86dc664e5e6fade274fc8ddcbafab5b81547
Osvaldo Torrez <nachi2083@gmail.com>
2021-11-16 18:24:17 -0300

    Sales report view

--- src/app/components/report-sales/report-sales.component.ts
+++ src/app/components/report-sales/report-sales.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faChevronLeft, faChevronRight, faFilePdf } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-report-sales',
@@ -7,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportSalesComponent implements OnInit {
 
-  public loading = true;
+  public loading: boolean = false;
+  public icons = [faFilePdf, faChevronLeft, faChevronRight];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  salesPrev(): void {
+    console.log('anterior');
+  }
+
+  salesNext(): void {
+    console.log('siguiente');
+  }
+
+  generatePdf(): void {
+    console.log('pdf');
   }
 
 }
